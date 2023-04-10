50cc50059bf37dbea85f4e6ea2c946aeb5ebe5ff
GramNaTosterze <krzysna@gmail.com>
2023-04-10 16:17:50 +0200

    Dodaj pliki projektów.

---
+++ PR_Lab4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+
+class PR_Lab4
+{
+    static void Main(string[] args)
+    {
+        int childArg = 0;
+        if (args.Length > 0)
+            childArg = int.Parse(args[0]);
+
+        if (childArg > 0)
+        {
+            var newProc = new ProcessStartInfo("PR_Lab4.exe");
+            newProc.Arguments = (childArg - 1).ToString();
+            newProc.UseShellExecute = true;
+            Process.Start(newProc);
+        }
+        for (int i = 0; i < 1000; i++)
+            Console.WriteLine(i);
+    }
+}
